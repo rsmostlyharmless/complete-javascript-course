c937d06ee3f05e4886628589b8eb7d8b5fcf315f
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-08-05 11:56:39 +0200

    notes added & tasks completed

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,96 @@
+// (END OF VIDEO TASKS)
+
+let country = 'Spain'
+const continent = 'Europe';
+let population = '46,792,561'
+const isIsland = false;
+let language;
+language = 'Spanish';
+language = 'Catalan';
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(language);
+
+// console.log('I live in ' + country + ' which is in ' + continent + ' and has a population of ' + population + '.')
+
+
+// (NOTES & PRACTICE)
+
+// JavaScriptIsFun = true;
+// console.log(JavaScriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof JavaScriptIsFun);
+// console.log(typeof 3);
+// console.log(typeof 'Ryan');
+
+// JavaScriptIsFun = 'YES!';
+// console.log(typeof JavaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1986;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null); legacy bug
+
+
+// let js = "amazing";
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Ryan');
+// console.log(36);
+
+// let person = 'Ryan';
+// let PI = 3.1415;
+
+// let myFirstJob = 'Cleaner';
+// let myCurrentJob = 'Student';
+
+// let job1 = 'cleaner';
+// let job2 = 'Student';
+
+// console.log('My first job was a ' + job1 + ' but now I am a ' + myCurrentJob);
+
+// console.log('Now I am a ' + job2 + ' but originally I was a ' + myFirstJob);
+
+
+// let firstName = 'Ryan';
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// camelCase is prefered
+
+// always start variables with lowercase
+
+// Don't use reserved key words
+
+// let years = 3;  no numbers at start of variables
+
+// let ryan&amy = 'RA'; variables can only has letters. numbers, _ or $. 
+
+// Number values do not need to be in quotations unless they use symbols/letters
+
+
+let age = 35;
+age = 36;
+
+const birthYear = 1986;
+// birthYear = 1989; not allowed with const
+
+// const job; const should alway be defined
+// var is the original variable and not really used
+var job = 'Student';
+job = 'programmer'
+// never declare a variable without const, let or var first
+lastName = 'Syder';
+console.log(lastName);
