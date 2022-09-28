e6f2bd13fde658dcbb7a171c140d3078f39d1e6c
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-09-28 18:08:04 +0200

    wathed video then repeated from memory

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,8 +2,21 @@
 
 const modal = document.querySelector(`.modal`);
 const overlay = document.querySelector(`.overlay`);
-const btnModal = document.querySelector(`.close-modal`);
+const btnCloseModal = document.querySelector(`.close-modal`);
 const btnsShowModal = document.querySelectorAll(`.show-modal`);
 
+const openModal = function () {
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener(`click`, openModal);
+
+btnCloseModal.addEventListener(`click`, closeModal);
+overlay.addEventListener(`click`, closeModal);
