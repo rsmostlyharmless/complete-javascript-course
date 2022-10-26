2cf08f9fba28b4981ff5fd51fc4722673f12a6a7
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-10-26 17:38:05 +0200

    first 4 challenges done. 3 to go

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,98 @@
 'use strict';
 
+// CHALLENGES
+
+// CHALLENGE 1
+/*
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game ('game' variable on next page). In this challenge we're gonna work with that data.
+*/
+// Your tasks:
+/*
+1. Create one player array for each team (variables 'players1' and 'players2').
+
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players.
+
+3. Create an array 'allPlayers' containing all players of both teams (22 players).
+
+4. Duringthegame,BayernMunich(team1)used3substituteplayers.Socreatea new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'.
+
+5. Based on the game.oddsobject, create one variable for each odd (called 'team1', 'draw' and 'team2').
+
+6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in).
+
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
+
+Test data for 6: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored.
+*/
+const game = {
+  team1: `Bayern Munich`,
+  team2: `Borrusia Dormund`,
+  players: [
+    [
+      `Neuer`,
+      `Pavard`,
+      `Martinez`,
+      `Alaba`,
+      `Davies`,
+      `Kimmich`,
+      `Goretzka`,
+      `Coman`,
+      `Muller`,
+      `Gnarby`,
+      `Lewandowski`,
+    ],
+    [
+      `Burki`,
+      `Schultz`,
+      `Hummels`,
+      `Akanji`,
+      `Hakimi`,
+      `Weigl`,
+      `Witsel`,
+      `Hazard`,
+      `Brandt`,
+      `Sancho`,
+      `Gotze`,
+    ],
+  ],
+  score: `4:0`,
+  scored: [`Lewandowski`, `Gnarby`, `Lewandowski`, `Hummels`],
+  date: `Nov 9th, 2037`,
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const players1 = [...game.players[0]];
+const players2 = [...game.players[1]];
+const gk = [players1[0]];
+const [, ...fieldPlayers] = [...game.players[0]];
+const allPlayers = [...game.players[0], ...game.players[1]];
+const player1Final = [...players1, `Thiago`, `Coutinho`, `Perisic`];
+const { x, team2, ...team1 } = game.odds;
+const {} = game.odds;
+// const team2 = { ...game.odds };
+
+// // Objects
+// const { sat, ...weekDays } = restaurant.openingHours;
+// console.log(weekDays);
+
+console.log(players1);
+console.log(players2);
+console.log(gk);
+console.log(fieldPlayers);
+console.log(allPlayers);
+console.log(player1Final);
+console.log(team1);
+console.log(draw);
+console.log();
+
+//
+//////////////////////////////////////////////////////////
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -145,7 +238,7 @@ const rest2 = {
 // console.log(a, b, others);
 
 // // Rest element must be last element
-// const [pizza, , risotto, ...otherFood /*, bread*/] = [
+// const [pizza, , risotto, ...otherFood /*, bread*/ //] = [
 //   ...restaurant.mainMenu,
 //   ...restaurant.starterMenu,
 // ];
