1470c53ea5166ada6677fd91a37702bc7d62337c
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-10-27 17:27:28 +0200

    notes done on enhanced obj literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -104,6 +104,22 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const weekdays = [`Mon`, `Tue`, `Wed`, `Thu`, `Fri`, `Sat`, `Sun`];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -111,20 +127,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // openingHours: openingHours, //// No longer used
+  // Enhanced Object Literals
+  openingHours, // now used
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -140,18 +145,23 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} & ${this.mainMenu[mainIndex]} to be sent to ${address} for ${time}`
     );
   },
-
+  // Old way of writing the sytax with the function ket word
   orderPasta: function (ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta made with ${ing1}, ${ing2} & ${ing3}`
     );
   },
-
-  orderPizza: function (mainIngr, ...otherIngr) {
+  // Enhanced Object Literals
+  // New syntax without function key word but both are exceptable
+  orderPizza(mainIngr, ...otherIngr) {
     console.log(mainIngr);
     console.log(otherIngr);
   },
 };
+console.log(restaurant);
+
+//
+//////////////////////////////////////////////
 //
 
 // FOR-OF LOOP
