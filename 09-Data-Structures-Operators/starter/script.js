d73adf3db5c75383776c7f3f1bcd9566f41eec34
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-11-04 16:17:50 +0100

    selctd corrct methds but needd prompts when building

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -187,6 +187,61 @@ const gameEvents = new Map([
 // }
 
 //
+// Challenge 4
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+
+Test data (pasted to textarea, including spaces):
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs): 
+underscoreCase ✅
+firstName ✅
+someVariable ✅
+calculateAge ✅
+delayedDeparture ✅
+
+Hints:
+- Remember which character defines a new line in the textarea 😉
+- The solution only needs to work for a variable made out of 2 words, like a_b
+- Start without worrying about the ✅. Tackle that only after you have the variable
+  name conversion working 😉
+- This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+
+document.body.append(document.createElement(`textarea`));
+document.body.append(document.createElement(`button`));
+
+const button = document.querySelector(`button`);
+
+button.addEventListener(`click`, function () {
+  const text = document.querySelector(`textarea`).value;
+  const rows = text.split(`\n`);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split(`_`);
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    const padding = `${output.padEnd(20)}${`🙈`.repeat(i + 1)}`;
+    console.log(padding);
+  }
+});
+*/
+/*
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+*/
+//
 //////////////////////////////////////////////////////////
 //
 //////////////////////////////////////////////////////////
