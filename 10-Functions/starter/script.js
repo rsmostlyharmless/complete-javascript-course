83ad9e264176436468b76744b26aab8d9ec85db1
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-11-10 15:36:38 +0100

    Challenge complete :(

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -2,7 +2,7 @@
 
 /*
 Your tasks:
-1. Create a method called' registerNewAnswer' on the 'poll' object. The method does 2 things:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
     1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this: What is your favourite programming language?
 
     0: JavaScript
@@ -26,12 +26,42 @@ Test data for bonus:
 
 Hints: Use many of the tools you learned about in this and the last section 😉
 */
+/*
+const pollEl = document.querySelector(`.poll`);
+
 const poll = {
   question: `What's your favourite programming language?`,
   options: [`0: JavaScript`, `1: Python`, `2: Rust`, `3: C++`],
   answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join(`\n`)}\n(Write down number)`
+      )
+    );
+
+    typeof answer === `number` &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults(`string`);
+  },
+  displayResults(type = `array`) {
+    if (type === `array`) {
+      console.log(this.answers);
+    } else if (type === `string`) {
+      console.log(`Poll results are ${this.answers.join(`, `)}`);
+    }
+  },
 };
 
+pollEl.addEventListener(`click`, poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, `string`);
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+*/
 //
 //////////////////////////////////////////////
 /////////////////////////////////////////////
