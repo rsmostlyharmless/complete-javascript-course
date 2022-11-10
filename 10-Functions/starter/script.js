9020bb8a806c8690d401aabc0b6ac2176e733de1
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-11-10 13:37:06 +0100

    notes made on bind method & set up challenge 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,42 @@
 'use strict';
 
+/*
+Your tasks:
+1. Create a method called' registerNewAnswer' on the 'poll' object. The method does 2 things:
+    1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this: What is your favourite programming language?
+
+    0: JavaScript
+    1: Python
+    2: Rust
+    3: C++
+
+    (Write option number)
+
+    1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?).
+2. Call this method when ever the user clicks the "Answerpoll" button.
+3. Create a method 'displayResults' which displays the poll results. The
+method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+Test data for bonus:
+
+§ Data1:[5,2,3]
+§ Data2:[1,5,3,9,6,1]
+
+Hints: Use many of the tools you learned about in this and the last section 😉
+*/
+const poll = {
+  question: `What's your favourite programming language?`,
+  options: [`0: JavaScript`, `1: Python`, `2: Rust`, `3: C++`],
+  answers: new Array(4).fill(0),
+};
+
+//
+//////////////////////////////////////////////
+/////////////////////////////////////////////
+//
+
 // Default perameters
 /*
 const logBooking = [];
@@ -164,9 +201,51 @@ console.log(swiss);
 
 book.call(euroWings, ...flightData);
 */
-
 //
 ////////////////////////////////////////
 //
 
 // The bind method
+/*
+const bookEW = book.bind(euroWings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(2948, `Stephen Stills`);
+bookLH(6857, `Wallace Wells`);
+bookLX(5743, `Ramona Flowers`);
+
+const bookEW4232 = book.bind(euroWings, 4232);
+bookEW4232(`Scott Pilgrim`);
+bookEW4232(`Kim Pine`);
+
+// With event listeners
+const buyEl = document.querySelector(`.buy`);
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+buyEl.addEventListener(`click`, lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = rate => value => value + value * rate;
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+// OR (per video)
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+*/
