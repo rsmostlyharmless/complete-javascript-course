d1f74e12dee47033545a5416522eb61943801215
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-11-10 18:02:37 +0100

    notes made on closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -303,3 +303,73 @@ runOnce();
 // as arrow function
 (() => console.log(`Dead End!!!`))();
 */
+
+//
+////////////////////////////////////////
+//
+
+// Closures
+/*
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+//
+////////////////////////////////////////
+//
+
+// More closures
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 42;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 333;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+// re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 5);
+*/
