b8bebe24998c98a080429974fc40b75a0ab7108e
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-11-23 13:58:23 +0100

    Challenge complete. Need to use forEach or forOf more

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -5,6 +5,7 @@
 // BANKIST APP
 
 // Data
+/*
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -79,6 +80,7 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+*/
 
 // console.log(containerMovements.innerHTML);
 
@@ -195,4 +197,69 @@ currenciesUnique.forEach(function (value, _, map) {
   //  duplicate parameters can be replaced by an under_score
   console.log(`${value}: ${value}`);
 });
+*/
+
+//
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+//
+
+// Challenge 1
+/*
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Your tasks:
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the first and the last two dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters).
+2. Create an array with both Julia's (corrected) and Kate's data.
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy")
+4. Run the function for both test data sets.Create
+
+Test data:
+- Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+- Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+Hints: Use tools from all lectures in this section so far 😉
+*/
+/*
+// My attempt with a little help with slice/splice methods
+const checkDogs = function (dogsJulia, dogsKate) {
+  const newDogsJulia = dogsJulia.slice();
+  newDogsJulia.splice(0, 1);
+  newDogsJulia.splice(-2);
+
+  const dogList = newDogsJulia.concat(dogsKate);
+
+  for (let i = 0; i < dogList.length; i++) {
+    const age = dogList[i];
+    const dog = age >= 3 ? `adult` : `puppy`;
+    const aAn = dog === `adult` ? `an` : `a`;
+    console.log(`This dog is ${age} yrs old and is ${aAn} ${dog}`);
+  }
+  console.log(newDogsJulia);
+  console.log(dogList);
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// The answer given in the challenge video
+const checkDogsJS = function (dogsJulia, dogsKate) {
+  const newDogsJulia = dogsJulia.slice();
+  newDogsJulia.splice(0, 1);
+  newDogsJulia.splice(-2);
+
+  const dogList = newDogsJulia.concat(dogsKate);
+
+  dogList.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is a ${dog} yr old adult`);
+    } else {
+      console.log(`Dog number ${i + 1} is a ${dog} yr old puppy`);
+    }
+  });
+
+  console.log(newDogsJulia);
+  console.log(dogList);
+};
+checkDogsJS([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 */
